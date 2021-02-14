f8d9402c16bd7746cd9b6205129baf36540ac3c5
Shane Harper <cdtharper@gmail.com>
2021-02-14 08:34:51 -0500

    user can input the number of asterisks for the system to output as well as the desired pattern

--- asterisks.java
+++ asterisks.java
@@ -9,7 +9,20 @@ public class asterisks {
         System.out.print( "\nNow enter a 1 or a 2 to represent if you want\nthe program to output the first or second pattern: " );
         int userPattern = input.nextInt();
 
-        System.out.println(numAsterisks);
-        System.out.println(userPattern);
+        if (userPattern == 1) {
+            for (int x = 1; x <= numAsterisks; x++) {
+                for (int y = 1; y <= x; y++) {
+                    System.out.print("*");
+                }
+                System.out.println("\n");
+            }
+        } else {
+            for (int x = numAsterisks; x > 0; x--) {
+                for (int y = x; y > 0; y--) {
+                    System.out.print("*");
+                }
+                System.out.println("\n");
+            }
+        }
     }
 }
